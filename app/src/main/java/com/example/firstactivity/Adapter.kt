39c38d9d091d7e647f43bd3f228e0144f57032ac
injae <hwanginjae411@gmail.com>
2023-02-20 22:10:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firstactivity/Adapter.kt
@@ -0,0 +1,34 @@
+package com.example.firstactivity
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(private val context: Context) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+    var datas = mutableListOf<MenuData>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_recycler,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val menu: TextView = itemView.findViewById(R.id.tv_rv_menu)
+
+        fun bind(item: MenuData) {
+            menu.text = item.menu
+
+        }
+    }
+}
+
+
+data class MenuData (val menu : String)
---
+++ app/src/main/java/com/example/firstactivity/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.firstactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var MenuAdapter: Adapter
+    private val datas = mutableListOf<MenuData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(/* layoutResID = */ R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(/* id = */ R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+
+        bottomNavigationView.setOnItemSelectedListener {
+            changeFragment(it.itemId)
+            true
+        }
+
+        changeFragment(R.id.home)
+    }
+
+
+
+    private fun changeFragment(menuItemId: Int) {
+        val ft = supportFragmentManager.beginTransaction()
+
+        when (menuItemId) {
+            R.id.home -> {
+                ft.replace(R.id.main_frame, Home()).commitAllowingStateLoss()
+//                initRecycler()
+            }
+            R.id.Profile -> ft.replace(R.id.main_frame, Person()).commitAllowingStateLoss()
+            R.id.Search -> ft.replace(R.id.main_frame, Search()).commitAllowingStateLoss()
+            R.id.Settings -> ft.replace(R.id.main_frame, Settings()).commitAllowingStateLoss()
+        }
+    }
+
+    private fun initRecycler() {
+
+        MenuAdapter = Adapter(Home)
+        findViewById<RecyclerView>(R.id.rv_item).adapter = MenuAdapter
+
+        datas.apply {
+            add(MenuData( menu = "mary",))
+            add(MenuData( menu = "hi",))
+            add(MenuData( menu = "hello",))
+
+            MenuAdapter.datas = datas
+            MenuAdapter.notifyDataSetChanged()
+        }
+    }
+}
